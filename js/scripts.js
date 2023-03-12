fccf96458e9b29ca009b9f0f759c70bd667789b1
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-03-12 18:58:37 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// let myName = 'Bob';
+// document.write(myName);
+// myName = 'John Doe';
+// document.write(myName);
+// alert(myName);
+
+// let size = 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
