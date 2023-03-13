64eed4d2d8381c5835afefcdbfedcc68bfa97d27
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-03-13 00:53:50 -0500

    cycle for
    I added cycle for to show Pokemon list on the website

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-let pokemonList=[{Name:: "Bulbasaur", Weight: "6.9kg", Type: [{'grass'}, {'poisson'}]},
-{Name: "Ivysaur", Weight: "13kg", Type: [{'grass'}, {'poison'}]},
-{Name: "Charmander", Weight: "8.5kg", Type: {'fire'}}
-]
+alert("Do you want to see the list pokemon?")
+var pokemonList = [{Name: "Bulbasaur", Weight: "6.9kg", Type: ['grass', 'poisson']},
+    {Name: "Ivysaur", Weight: "13kg", Type: ['grass', 'poison']},
+    {Name: "Charmander", Weight: "8.5kg", Type: 'fire'},
+];
 
+    // alert("Click here to see pokemin list");
+    
+document.write("<ul>");
+
+for (var i=0; i<pokemonList.length; i++) {
+    document.write(`<li>${pokemonList[i].Name} - Weight: ${pokemonList[i].Weight} - Type: ${pokemonList[i].Type.join(", ")}</li>`);
+}
+  
+document.write("</ul>");
+  
