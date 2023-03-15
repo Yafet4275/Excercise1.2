d3d111ffe48fe25eb7a4c694f125cdd4192be33f
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-03-14 21:53:14 -0500

    Adding wow how big!

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 
-var pokemonList = [{name: "Bulbasaur", weight: "6.9kg", types: ['grass', 'poisson']},
-    {name: "Ivysaur", weight: "13kg", types: ['grass', 'poison']},
-    {name: "Charmander", weight: "8.5kg", types: 'fire'},
+let repository = [
+    {name: "Bulbasaur",
+    weight: "6.9kg",
+    height: "0.7",
+    types: ['grass', 'poisson']},
+
+    {name: "Ivysaur",
+    weight: "13kg",
+    height: "1.7",
+    types: ['grass', 'poison']},
+
+    {name: "Charmander",
+    height: 1,
+    weight: "8.5kg",
+    types: 'fire'},
 ];
 
 document.write("<ul>");
 
-for (var i=0; i<pokemonList.length; i++) {
-    document.write(`<li>${pokemonList[i].name} - weight: ${pokemonList[i].weight} - types: ${pokemonList[i].types.join(", ")}</li>`);
+for (var i=0; i<repository.length; i++) {
+    var pokemon = repository[i];
+    if (pokemon.height >= '1.7') {
+        console.log(pokemon.name);
+        var nameString = "This pokemon's name is " + pokemon.name + ", “Wow, that’s big!”";
+    } else {
+        var nameString = "This pokemon's name is " + pokemon.name;
+    }
+    document.write('<li>' + nameString + '</li>');
 }
   
 document.write("</ul>");
