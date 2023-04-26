40c348e8a91e93e2c98191046d4e62effc0d712c
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-04-25 21:31:43 -0500

    Initial commit Dom
    This is the initial commit for Dom branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
-let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur', type: ['grass', 'poison'], height: 0.7},
-    {name: 'Charmander', type: ['fire'], height: 0.6},
-    {name: 'Squirtle', type: ['water'], height: 0.5}
-  ];
+// Pokemon list array
+let pokemonList = [
+  { name: 'Bulbasaur', type: 'grass', level: 10 },
+  { name: 'Charmander', type: 'fire', level: 8 },
+  { name: 'Squirtle', type: 'water', level: 7 }
+];
 
-  return {
-    getAll: function() {
-      return pokemonList;
-    },
-    add: function(newPokemon) {
-      if (typeof newPokemon === 'object' && Object.keys(newPokemon).length === 3 && 
-          typeof newPokemon.name === 'string' && typeof newPokemon.type === 'object' &&
-          Array.isArray(newPokemon.type) && newPokemon.type.length > 0 &&
-          typeof newPokemon.height === 'number') {
-        pokemonList.push(newPokemon);
-      } else {
-        console.log('You can only add an object with keys "name", "type", and "height" to the pokemonList.');
-      }
-    },
-    findByName: function(name) {
-      let filteredList = pokemonList.filter(function(pokemon) {
-        return pokemon.name.toLowerCase() === name.toLowerCase();
+// Select element
+let pokemonListElement = document.querySelector('.pokemon-list');
+
+// Define the IIFE
+let pokemonApp = (function() {
+  function addListItem(pokemon) {
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    // console.log(listItem);
+    pokemonListElement.appendChild(listItem);
+    button.addEventListener('click', function() {
+        showDetails(pokemon);
       });
-      return filteredList.length ? filteredList[0] : null;
-    }
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  // Loop through the pokemonList array and call the addListItem function for each object
+  pokemonList.forEach(function(pokemon) {
+    addListItem(pokemon);
+  });
+  
+  // Return an object with the addListItem function as a method
+  return {
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.add({name: 'NewPokemon', type: ['newFeature'], height: 0.3});
-pokemonRepository.add('Invalid data');
-
-let pokemonListElement = document.getElementById('pokemonList');
-      pokemonRepository.getAll().forEach(function(pokemon) {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = '<h2>' + pokemon.name + '</h2><p>Type: ' + pokemon.type.join(', ') + '</p><p>Height: ' + pokemon.height + '</p>';
-        pokemonListElement.appendChild(listItem);
-      });
-
-console.log(pokemonRepository.findByName('bulbasaur'));
-console.log(pokemonRepository.findByName('Pikachu'));
+// Call the addListItem method outside of the IIFE and pass a Pokemon object as a parameter
+pokemonApp.addListItem({ name: 'Pikachu', type: 'electric', level: 12 });
